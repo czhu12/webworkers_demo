d9ece8935455d7cfdfaf17f005d175ea03aada39
chris <chris.zhu12@gmail.com>
2014-05-17 16:33:40 -0400

    refactoring

--- public/work.js
+++ public/work.js
@@ -3,9 +3,8 @@ self.interval;
 
 self.addEventListener('message', function(e){
   if(e.data.command === 'start'){
-    console.log('setting interval');
     self.interval = setInterval(function(){
-      console.log('hello' + e.data.id);
+      self.postMessage('hello from ' + e.data.id);
     }, 1000);
   } else if(e.data.command === 'stop'){
     clearInterval(self.interval);
