1b8939052f0b432667f9b7307cf174d820dff0ad
Chris Zhu <chriszhu@fb.com>
2014-05-19 08:54:33 -0700

    set up data flow

--- public/work.js
+++ public/work.js
@@ -1,14 +1,18 @@
-
-self.interval;
-
 self.addEventListener('message', function(e){
-  if(e.data.command === 'start'){
-    self.interval = setInterval(function(){
-      self.postMessage('hello from ' + e.data.id);
-    }, 1000);
-  } else if(e.data.command === 'stop'){
-    clearInterval(self.interval);
-  }
+  var answer = work(e.data.param, 0, '');
+  self.postMessage({param: e.data.param, value: answer});
 }, false);
 
+function work(n, acc, acc2){
+  acc = acc + 1;
+  acc2 = n + acc2;
+  if(n <= 4){
+    return acc2;
+  }
 
+  if((n % 2) === 0){
+    return work(n/2, acc, acc2);
+  }else{
+    return work(3 * n + 1, acc, acc2); 
+  }
+}
