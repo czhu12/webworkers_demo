b1439a9a609e06328c6b991e92b84e0b7960e5e5
Chris Zhu <chriszhu@fb.com>
2014-05-19 10:54:32 -0700

    test worked!

--- public/work.js
+++ public/work.js
@@ -1,6 +1,7 @@
 self.addEventListener('message', function(e){
-  var answer = work(e.data.param, 0, '');
-  self.postMessage({param: e.data.param, value: answer});
+  //var answer = work(e.data.param, 0, '');
+  var answer = incr(e.data.param.param);
+  self.postMessage({param: e.data.param.param, value: answer, id: e.data.id});
 }, false);
 
 function work(n, acc, acc2){
@@ -15,4 +16,12 @@ function work(n, acc, acc2){
   }else{
     return work(3 * n + 1, acc, acc2); 
   }
+}
+
+function incr(n){
+  
+  while(n < 2147483648){
+    n = n + 1;
+  }
+  return n;
 }
